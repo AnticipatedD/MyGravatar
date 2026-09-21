21f64e80d947c904c745d67f4c077b5146798fc0
AnticipatedD <mdabul@cc.cc>
2026-09-21 15:16:50 +0600

    feat(logger): add structured JSON logger utility

---
+++ utils/logger.py
@@ -0,0 +1,13 @@
+import logging
+import sys
+from pythonjsonlogger import jsonlogger
+
+def get_structured_logger(name: str):
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stdout)
+        formatter = jsonlogger.JsonFormatter('%(asctime)s %(levelname)s %(name)s %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.setLevel(logging.INFO)
+    return logger
